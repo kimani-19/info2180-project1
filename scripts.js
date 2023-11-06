d598a4dc25694e471604032a95e119fe36e6c5ab
kimani-19 <147687023+kimani-19@users.noreply.github.com>
2023-11-05 23:08:32 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
 /* Add your JavaScript to this file */
+window.onload=function(){
+    var button = document.getElementsByClassName("btn")[1];
+    button.addEventListener("click", function(e){
+        var email
+        var Text = document.getElementsByClassName("message")[0];
+        var passMessage = "Thank you! Your email address" + email + "has been to mailing list";
+        var errorMessage = "Please enter a valid email address";
+
+        if (email.length ==0){
+            Text.textContent= errorMessage;
+        }
+
+        else if(email.length > 0){
+            Text.textContent=passMessage;
+        }
+        e.preventDefault();
+    });
+}
